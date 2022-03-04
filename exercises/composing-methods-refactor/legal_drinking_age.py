a62307c35919deb714f52f5a7de20359cf36911d
aldrinbrillante <69699828+aldrinbrillante@users.noreply.github.com>
2022-03-04 14:01:43 -0800

    transferred local edit to github

--- exercises/composing-methods-refactor/legal_drinking_age.py
+++ exercises/composing-methods-refactor/legal_drinking_age.py
@@ -1,25 +1,17 @@
-# by Kami Bigdely
-# Inline method.
 # TODO: Refactor this program to improve its readability.
 
 LEGAL_DRINKING_AGE = 18
+
+
 class Person:
     def __init__(self, my_age):
         self.age = my_age
-        
-def enter_night_club(individual):
-    if older_than_18_year_old(individual.age):
-        print("Allowed to enter.")
-    else:
-        print("Enterance of minors is denited.")
 
-def older_than_18_year_old(age):
-    if age > LEGAL_DRINKING_AGE:
-        return True
-    else: 
-        return False
-    
-    
-person = Person(17.9)
+
+def enter_night_club(individual):
+    print("Allowed to enter.") if individual.age >= LEGAL_DRINKING_AGE else print(
+        "Enterance of minors is denited.")
+
+
+person = Person(23)
 enter_night_club(person)
-        
